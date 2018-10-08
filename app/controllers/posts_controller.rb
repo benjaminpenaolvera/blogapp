31ee2be3bd86eacc65c73e91bd3f483febc6ce08
Benjamin Peña Olvera <benjamin.pena.olvera@gmail.com>
2018-10-08 16:53:23 -0500

    post controller edit and update actions with views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,31 +1,44 @@
 class PostsController < ApplicationController
   before_action :authenticate_admin_user!, except: [:index, :show]
+  before_action :find_post, only: [:show, :edit, :update]
 
   def index
     @posts = Post.all
   end
 
-  def show
-    @post = Post.friendly.find(params[:id])
-  end
+  def show; end
 
   def new
     @post = Post.new
-    binding.pry
   end
 
   def create
-    binding.pry
     @post = Post.new(post_params)
     if @post.valid?
       @post.save
+      flash[:success] = 'Post created!'
       redirect_to root_url, notice: 'Post created'
     else
       render :new
     end
   end
 
+  def edit; end
+
+  def update
+    if @post.update_attributes(post_params)
+      flash[:notice] = 'Post updated'
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+  def find_post
+    @post = Post.friendly.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :content, :tags, :status, :header)
