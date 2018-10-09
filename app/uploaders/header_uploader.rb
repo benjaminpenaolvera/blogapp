943628245e98f269512818c8ca5d6e1c5031a5b1
Benjamin Peña Olvera <benjamin.pena.olvera@gmail.com>
2018-10-08 21:11:00 -0500

    landing index posts

--- app/uploaders/header_uploader.rb
+++ app/uploaders/header_uploader.rb
@@ -33,6 +33,10 @@ class HeaderUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [50, 50]
   end
 
+  version :header do
+    process resize_to_fit: [900, 500]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
